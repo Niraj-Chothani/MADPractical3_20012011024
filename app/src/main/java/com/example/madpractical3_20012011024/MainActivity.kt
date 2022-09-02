88479943d8a65cf8e55ce363514d98f3e0260425
Niraj-Chothani <nirajchothani20@gnu.ac.in>
2022-09-02 08:20:49 +0530

    updated

--- app/src/main/java/com/example/madpractical3_20012011024/MainActivity.kt
+++ app/src/main/java/com/example/madpractical3_20012011024/MainActivity.kt
@@ -8,13 +8,30 @@ import com.example.madpractical3_20012011024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-        Intent(this,LoginActivity::class.java).also { startActivity(it) }
+        binding.bottomNavigationView.setOnItemSelectedListener { it1 ->
+            when(it1.itemId)
+            {
+                R.id.bottom_nav_login->
+                {
+                    Intent(this,LoginActivity::class.java).also { startActivity(it) }
+                }
+                R.id.bottom_nav_reg->
+                {
+                    Intent(this,RegistrationActivity::class.java).also { startActivity(it) }
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
